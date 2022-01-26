b38085620feca3d0d8137e7f458ed43ee097040d
TheBoiThatHasOOF <92058789+TheBoiThatHasOOF@users.noreply.github.com>
2022-01-26 16:52:52 -0700

    Create gui.js

---
+++ gui.js
@@ -0,0 +1,10 @@
+window.alert("InjectGUI By TheBoiThatHasOOF")
+
+if (window.prompt('Run A CMD') == 'inject') {
+  window.alert('Injected');
+} else {
+}
+
+if (window.prompt('Run A CMD') == 'bruh') {
+  window.alert('bruh');
+}
